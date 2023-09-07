6a8c79cb838c287ea1362e9d25e23fa5db7cbf2c
“abdlqdr” <qdrkhan9@gmail.com>
2023-09-07 18:18:45 +0530

    new app committ 3

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -26,15 +26,22 @@ export class Navbar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
+                  <a className="nav-link " aria-current="page" href="/">
                     Home
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    About
-                  </a>
-                </li>
+                  <a className="nav-link" href="/business">Business</a></li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/entertainment">Entertainment</a></li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/health">Health</a></li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/science">Science</a></li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/sports">Sports</a></li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/technology">Technology</a></li>
               </ul>
               <form className="d-flex">
                 <input
